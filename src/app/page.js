21e6bc5b8e8eb83337e4108448de6c7cd3b82ac0
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-01-23 14:38:54 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -28,15 +28,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
-              Smart Space Builder Automations
-            </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-              Quisquemos sodales suscipit tortor ditaemcos condimentum de cosmo
-              lacus meleifend menean diverra loremous.
-            </p>
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+  <div className="mr-auto place-self-center lg:col-span-7">
+    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+      Smart Space Builder Automations
+    </h1>
+    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+      Quisquemos sodales suscipit tortor ditaemcos condimentum de cosmo
+      lacus meleifend menean diverra loremous.
+    </p>
 
             <button
               type="button"
