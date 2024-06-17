4e425c69b58a82ab3a4a30dadfdc9174749abf23
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-06-17 10:49:07 +0530

    Commit

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -89,6 +89,7 @@ export default function Login() {
                     [controlItem.id]: event.target.value,
                   })
                 }
+                inputClassName="w-full px-3 py-2 text-gray-900 border border-gray-300 rounded-md focus:outline-none focus:border-black"
               />
             ) : null
           )}
