865f4b47082884e26d03ccd63b5a085c79d4bc97
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-06-20 10:47:06 +0530

    Commit

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -90,7 +90,7 @@ export default function Login() {
                   })
                 }
                 inputClassName="w-full px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:border-black"
-                style={{ color: "black" }}  // Ensure text color is black
+                style={{ color: "black", backgroundColor: "white" }} // Ensure text color is black
               />
             ) : null
           )}
