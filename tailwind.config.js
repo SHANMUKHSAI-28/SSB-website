66a890194cdc0ea1741b5bcc3726936bc7fce45b
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-06-20 10:40:19 +0530

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -10,15 +9,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        black: '#000000',
-      },
-    },
-    textColor: {
-      primary: '#000000',
     },
   },
   plugins: [],
-};
+}
