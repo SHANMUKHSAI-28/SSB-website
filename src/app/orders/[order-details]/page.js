33e879e0e31c742d6bfd1958e5b6afe569ec3434
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-06-20 11:29:51 +0530

    Commit

--- src/app/orders/[order-details]/page.js
+++ src/app/orders/[order-details]/page.js
@@ -16,7 +16,7 @@ export default function OrderDetails() {
   } = useContext(GlobalContext);
 
   const params = useParams();
-  const router = useRouter()
+  const router = useRouter();
 
   async function extractOrderDetails() {
     setPageLevelLoader(true);
@@ -56,7 +56,7 @@ export default function OrderDetails() {
         <h1 className="text-3xl lg:text-4xl font-bold leading-7 lg:leading-9 text-gray-900">
           Order #{orderDetails && orderDetails._id}
         </h1>
-        <p className="text-base font-medium leadong-6 text-gray-600">
+        <p className="text-base font-medium leading-6 text-gray-600">
           {orderDetails &&
             orderDetails.createdAt &&
             orderDetails.createdAt.split("T")[0]}{" "}
@@ -69,7 +69,7 @@ export default function OrderDetails() {
       <div className="mt-10 flex flex-col justify-center xl:flex-row items-stretch w-full xl:space-x-8 md:space-y-6 xl:space-y-0">
         <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
           <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:p-6 xl:p-8 w-full">
-            <p className="font-bol text-lg ">Your order summary</p>
+            <p className="font-bold text-lg text-black">Your order summary</p>
             {orderDetails &&
             orderDetails.orderItems &&
             orderDetails.orderItems.length
@@ -92,7 +92,7 @@ export default function OrderDetails() {
                       </div>
                       <div className="w-full flex justify-between items-start space-x-8">
                         <h3 className="text-xl font-semibold leading-6 text-gray-900">
-                        ₹{item && item.product && item.product.price}
+                          ₹{item && item.product && item.product.price}
                         </h3>
                       </div>
                     </div>
@@ -102,14 +102,14 @@ export default function OrderDetails() {
           </div>
           <div className="flex justify-center flex-col md:flex-row items-stretch w-full space-y-4 md:space-y-0 md:space-x-5 xl:space-x-8">
             <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6">
-              <h3 className="text-xl font-semibold leading-6 text-gray-900">
+              <h3 className="text-xl font-semibold leading-6 text-black">
                 Summary
               </h3>
               <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
                 <div className="flex justify-between w-full">
                   <p className="text-base leading-5 text-gray-800">Subtotal</p>
                   <p className="text-base leading-5 text-gray-900">
-                  ₹{orderDetails && orderDetails.totalPrice}
+                    ₹{orderDetails && orderDetails.totalPrice}
                   </p>
                 </div>
                 <div className="flex justify-between w-full">
@@ -119,7 +119,7 @@ export default function OrderDetails() {
                 <div className="flex justify-between w-full">
                   <p className="text-base leading-5 text-gray-800">Subtotal</p>
                   <p className="text-base leading-5 text-gray-900">
-                  ₹{orderDetails && orderDetails.totalPrice}
+                    ₹{orderDetails && orderDetails.totalPrice}
                   </p>
                 </div>
               </div>
@@ -127,35 +127,35 @@ export default function OrderDetails() {
           </div>
         </div>
         <div className="flex flex-col gap-5">
-          <div className="bg-gray-50 w-full xl:w-96 flex  items-center md:items-start px-4 py-6 flex-col">
-            <h3 className="text-xl font-semibold leading-6 text-gray-900">
+          <div className="bg-gray-50 w-full xl:w-96 flex items-center md:items-start px-4 py-6 flex-col">
+            <h3 className="text-xl font-semibold leading-6 text-black">
               Customer Details
             </h3>
             <div className="flex flex-col justify-start items-start flex-shrink-0">
-              <div className="flex gap-4 justify-center flex-col w-full md:justify-start   py-8 border-b border-gray-200">
-                <p className="text-base font-semibold leading-4 text-left text-gray-950">
+              <div className="flex gap-4 justify-center flex-col w-full md:justify-start py-8 border-b border-gray-200">
+                <p className="text-base font-semibold leading-4 text-left text-black">
                   Name: {user?.name}
                 </p>
-                <p className="text-base font-semibold leading-4 text-left text-gray-950">
+                <p className="text-base font-semibold leading-4 text-left text-black">
                   Email: {user?.email}
                 </p>
               </div>
             </div>
           </div>
           <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
-            <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 md:space-y-0 xl:space-y-12 md:flex-row items-center md:items-start ">
+            <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 md:space-y-0 xl:space-y-12 md:flex-row items-center md:items-start">
               <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
-                <p>Shipping Address</p>
-                <p>
+                <p className="text-black">Shipping Address</p>
+                <p className="text-black">
                   Address :{" "}
                   {orderDetails && orderDetails.shippingAddress.address}
                 </p>
-                <p>City :{orderDetails && orderDetails.shippingAddress.city}</p>
-                <p>
+                <p className="text-black">City :{orderDetails && orderDetails.shippingAddress.city}</p>
+                <p className="text-black">
                   Country :{" "}
                   {orderDetails && orderDetails.shippingAddress.country}
                 </p>
-                <p>
+                <p className="text-black">
                   Postal Code :{" "}
                   {orderDetails && orderDetails.shippingAddress.postalCode}
                 </p>
@@ -164,7 +164,7 @@ export default function OrderDetails() {
           </div>
           <button
             onClick={() => router.push(`/`)}
-            className="mt-5 mr-5  inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+            className="mt-5 mr-5 inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
           >
             Shop Again
           </button>
