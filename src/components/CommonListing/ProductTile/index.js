7d8f39241a0414defdba672f31788108ff4d6ece
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-06-20 15:59:13 +0530

    Commit

--- src/components/CommonListing/ProductTile/index.js
+++ src/components/CommonListing/ProductTile/index.js
@@ -6,8 +6,8 @@ export default function ProductTile({ item }) {
   const router = useRouter();
   
   return (
-    <div  onClick={()=> router.push(`/product/${item._id}`)}>
-      <div className="overflow-hideen aspect-w-1 aspect-h-1 h-52">
+    <div onClick={() => router.push(`/product/${item._id}`)}>
+      <div className="overflow-hidden aspect-w-1 aspect-h-1 h-52">
         <img
           src={item.imageUrl}
           alt="Product image"
@@ -16,7 +16,7 @@ export default function ProductTile({ item }) {
       </div>
       {item.onSale === "yes" ? (
         <div className="absolute top-0 m-2 rounded-full bg-black">
-          <p className="rounded-full  p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">
+          <p className="rounded-full p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">
             Sale
           </p>
         </div>
@@ -24,7 +24,7 @@ export default function ProductTile({ item }) {
       <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
         <div className="mb-2 flex">
           <p
-            className={`mr-3 text-sm font-semibold ${
+            className={`mr-3 text-sm font-semibold text-black ${
               item.onSale === "yes" ? "line-through" : ""
             }`}
           >{`₹ ${item.price}`}</p>
@@ -35,7 +35,7 @@ export default function ProductTile({ item }) {
             ).toFixed(2)}`}</p>
           ) : null}
           {item.onSale === "yes" ? (
-            <p className="mr-3 text-sm font-semibold">{`-(${item.priceDrop}%)off`}</p>
+            <p className="mr-3 text-sm font-semibold">{`-(${item.priceDrop}%) off`}</p>
           ) : null}
         </div>
         <h3 className="md-2 text-gray-400 text-sm">{item.name}</h3>
