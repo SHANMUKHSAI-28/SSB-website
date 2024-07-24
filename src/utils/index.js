2a2df92e907f8ffc30e0e1f005586c51bd27134f
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-07-24 15:02:01 +0530

    Commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,7 +18,7 @@ export const navOptions = [
   { id: "internships", label: "Internships", path: "https://forms.gle/RTKTFVtL5YzTUMKQ7" },
   { id: "contact-us", label: "Contact Us", path: "https://docs.google.com/forms/d/e/1FAIpQLScj52_62GUDX1FPdBsm-HPuw_CgH9l5RmVxh-ZNuBSIYfoxVA/viewform?usp=sf_link" },
   { id: "aboutus", label: "About Us", path: "/aboutus" },
-  { id: "Verify", label: "Verify Certificate", path: "/aboutus" }, 
+  { id: "Verify", label: "Verify", path: "/aboutus" }, 
 ];
 
 export const adminNavOptions = [
