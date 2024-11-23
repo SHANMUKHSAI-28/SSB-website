3088c63532f379faea5e4da8fcd563fea73bf539
SHANMUKH SAI <saishanmuk28@gmail.com>
2024-11-23 22:19:57 +0530

    logo

--- src/app/page.js
+++ src/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
         <h2 className="text-4xl font-bold text-center mb-10">💻 Our Expertise</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {[
-            { title: "Embedded Systems", icon: "/icons/embedded.svg" },
+            { title: "Embedded Systems", icon: "images/embedded.jpg" },
             { title: "VLSI", icon: "/icons/vlsi.svg" },
             { title: "App Development", icon: "/icons/app.svg" },
             { title: "IoT Solutions", icon: "/icons/iot.svg" },
